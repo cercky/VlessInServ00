41843abe6f5e0a650463b2af170cbb241540b374
cercky <cercky@qq.com>
2024-06-30 23:38:42 +0800

    fix start
    更新启动方式

--- vless/start.sh
+++ vless/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
- 
-nohup node vless/vless.js > logs/start_node.log 2>&1 &
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+nohup /usr/local/bin/node $DIR/vless.js > $DIR/logs/start_node.log 2>&1 &
