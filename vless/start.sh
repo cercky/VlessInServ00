fb6092fa3ab6e4675b8a8cd322521ba063dca7d9
cercky <cercky@qq.com>
2024-07-01 00:02:55 +0800

    fix start

--- vless/start.sh
+++ vless/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-nohup /usr/local/bin/node $DIR/vless.js > $DIR/logs/start_node.log 2>&1 &
+nohup /usr/local/bin/node $DIR/vless.js &
